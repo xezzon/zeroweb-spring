b8220fe2303e0fbccefc7e9de5b11455b533010a
xezzon <xezzon@outlook.com>
2024-10-16 17:33:19 +0800

    移除 QueryDSL 依赖

--- geom-spring-boot-starter/src/test/java/io/github/xezzon/geom/common/jpa/TestEntityDAO.java
+++ geom-spring-boot-starter/src/test/java/io/github/xezzon/geom/common/jpa/TestEntityDAO.java
@@ -1,6 +1,5 @@
 package io.github.xezzon.geom.common.jpa;
 
-import com.querydsl.core.types.dsl.EntityPathBase;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Repository;
@@ -12,17 +11,12 @@ import org.springframework.stereotype.Repository;
 public class TestEntityDAO extends BaseDAO<TestEntity, String, TestEntityRepository> {
 
   protected TestEntityDAO(TestEntityRepository repository) {
-    super(repository);
+    super(repository, TestEntity.class);
   }
 
   @Override
   public ICopier<TestEntity> getCopier() {
     return Copier.INSTANCE;
-  }
-
-  @Override
-  protected EntityPathBase<TestEntity> getQuery() {
-    return QTestEntity.testEntity;
   }
 
   @Mapper
