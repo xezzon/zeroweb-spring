9146ec7cc84bdbdbd8cedb52fdb8ea61245cf623
xezzon <xezzon@outlook.com>
2025-07-15 14:47:21 +0800

    添加默认 Bean 注解防止启动失败。

--- zeroweb-spring-boot-starter/src/main/java/io/github/xezzon/zeroweb/metadata/DefaultMenuService.java
+++ zeroweb-spring-boot-starter/src/main/java/io/github/xezzon/zeroweb/metadata/DefaultMenuService.java
@@ -2,12 +2,14 @@ package io.github.xezzon.zeroweb.metadata;
 
 import java.util.Collections;
 import java.util.List;
+import org.springframework.context.annotation.Fallback;
 import org.springframework.stereotype.Service;
 
 /**
  * @author xezzon
  */
 @Service
+@Fallback
 public class DefaultMenuService implements IMenuService {
 
   @Override
