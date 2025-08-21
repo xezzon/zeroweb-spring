c6fc4b75df8a24a33010da2835535dfd8874e338
xezzon <xezzon@outlook.com>
2025-08-21 11:10:09 +0800

    新增`附件管理服务`模块。

---
+++ zeroweb-service/zeroweb-service-file/src/main/java/io/github/xezzon/zeroweb/ZerowebFileApplication.java
@@ -0,0 +1,24 @@
+package io.github.xezzon.zeroweb;
+
+import io.github.xezzon.zeroweb.dict.EnableDictScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+
+/**
+ * 附件管理服务
+ * @author xezzon
+ */
+@SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+@EnableDictScan
+public class ZerowebFileApplication {
+
+  private ZerowebFileApplication() {
+  }
+
+  static void main(String[] args) {
+    SpringApplication.run(ZerowebFileApplication.class, args);
+  }
+}
