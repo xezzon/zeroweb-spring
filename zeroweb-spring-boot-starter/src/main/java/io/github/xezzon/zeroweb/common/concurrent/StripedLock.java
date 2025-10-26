9819b418c2d493009e69b0f6daa219869d66c08e
xezzon <xezzon@outlook.com>
2025-10-26 20:37:22 +0800

    新增 LockAdaptor 和 LockProvider 接口及其实现 StripedLock，提供资源加锁功能

---
+++ zeroweb-spring-boot-starter/src/main/java/io/github/xezzon/zeroweb/common/concurrent/StripedLock.java
@@ -0,0 +1,53 @@
+package io.github.xezzon.zeroweb.common.concurrent;
+
+import com.google.common.util.concurrent.Striped;
+import io.github.xezzon.zeroweb.common.exception.ZerowebRuntimeException;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.function.Supplier;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Fallback;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author xezzon
+ */
+@Component
+@Fallback
+public class StripedLock implements LockProvider {
+
+  private final Striped<@NotNull Lock> stripedLocks = Striped.lazyWeakLock(10);
+
+  @Override
+  public LockAdaptor of(String name, int timeout) {
+    return new InnerLock(timeout);
+  }
+
+  public class InnerLock implements LockAdaptor {
+
+    private final int timeout;
+
+    public InnerLock(int timeout) {
+      this.timeout = timeout;
+    }
+
+    @Override
+    public <R> Optional<R> tryLock(String id, Supplier<R> supplier) {
+      Lock specificLock = stripedLocks.get(id);
+      try {
+        if (!specificLock.tryLock(timeout, TimeUnit.SECONDS)) {
+          return Optional.empty();
+        }
+        try {
+          return Optional.ofNullable(supplier.get());
+        } finally {
+          specificLock.unlock();
+        }
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        throw new ZerowebRuntimeException(e);
+      }
+    }
+  }
+}
