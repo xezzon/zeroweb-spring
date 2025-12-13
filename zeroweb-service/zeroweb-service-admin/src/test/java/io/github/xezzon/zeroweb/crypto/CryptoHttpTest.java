8cf269ee4295f843b80eada2e0741369c00270a4
xezzon <xezzon@outlook.com>
2025-12-13 20:44:02 +0800

    新增获取 JWKs 接口。

--- zeroweb-service/zeroweb-service-admin/src/test/java/io/github/xezzon/zeroweb/crypto/CryptoHttpTest.java
+++ zeroweb-service/zeroweb-service-admin/src/test/java/io/github/xezzon/zeroweb/crypto/CryptoHttpTest.java
@@ -1,8 +1,17 @@
 package io.github.xezzon.zeroweb.crypto;
 
 import cn.hutool.core.util.RandomUtil;
+import io.github.xezzon.zeroweb.core.crypto.PemClasspathReaderAndWriter;
+import io.github.xezzon.zeroweb.core.crypto.SecretKeyUtil;
+import io.jsonwebtoken.security.EcPublicJwk;
+import io.jsonwebtoken.security.Jwk;
+import io.jsonwebtoken.security.JwkSet;
+import io.jsonwebtoken.security.Jwks;
 import jakarta.annotation.Resource;
+import java.security.PublicKey;
+import java.security.interfaces.ECPublicKey;
 import java.util.Map;
+import org.bouncycastle.openssl.PEMException;
 import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -42,5 +51,27 @@ class CryptoHttpTest {
     Double guessesLog10 = Assertions
         .assertInstanceOf(Double.class, responseBody.get("guessesLog10"));
     Assertions.assertTrue(guessesLog10 > 0);
+  }
+
+  @Test
+  void wellKnownJwks() throws PEMException {
+    String responseBody = testClient.get()
+        .uri("/well-known/jwks.json")
+        .exchange()
+        .expectStatus().isOk()
+        .expectBody(String.class)
+        .returnResult().getResponseBody();
+    Assertions.assertNotNull(responseBody);
+    JwkSet jwkSet = Jwks.setParser().build().parse(responseBody);
+    Assertions.assertEquals(1, jwkSet.size());
+    Jwk<?> jwk = jwkSet.getKeys().stream()
+        .findFirst()
+        .orElseThrow();
+    EcPublicJwk ecPublicJwk = Assertions.assertInstanceOf(EcPublicJwk.class, jwk);
+    ECPublicKey actual = ecPublicJwk.toKey();
+    PemClasspathReaderAndWriter pemReaderAndWriter =
+        new PemClasspathReaderAndWriter("pem/xezzon.github.io");
+    PublicKey expect = SecretKeyUtil.readPublicKey(pemReaderAndWriter);
+    Assertions.assertEquals(expect, actual);
   }
 }
